51b58dcf659398626bd893e623112bc0d33d6769
Mohammed Mostafa <77961766+Mohammed-Mostafa-Hasan@users.noreply.github.com>
2023-04-29 21:35:30 +0200

    Update Booking-app.py

--- Booking-app.py
+++ Booking-app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 from sklearn.preprocessing import LabelEncoder
+import joblib
+
 
 st.write("""
 # Booking Prediction App
